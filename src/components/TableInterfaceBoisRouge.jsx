2fbe2c375f322e36d66b0e29d3140c476503aa40
Amir Nadji <amir.nadji@coretix.io>
2023-09-07 09:43:07 +0100

    Multiple textures (plus que 2)

--- src/components/TableInterfaceBoisRouge.jsx
+++ src/components/TableInterfaceBoisRouge.jsx
@@ -7,6 +7,7 @@ import {
 } from "@mui/material";
 import Table1 from '../assets/couleur1.png'
 import Table2 from '../assets/couleur2.png'
+import Table3 from '../assets/couleur3.png'
 
 const itemData = [
     {
@@ -18,6 +19,11 @@ const itemData = [
         img: Table2,
         title: 'Table bois rouge',
         texture: 'texture2'
+    },
+    {
+        img: Table3,
+        title: 'Table bois rouge',
+        texture: 'texture3'
     },
 ];
 import { useGlobalContext } from "../context";
@@ -34,13 +40,13 @@ export const TableInterfaceBoisRouge = () => {
         >
             <Stack spacing={3}>
                 <Typography variant="h6">Produits</Typography>
-                <Box className="glass" p={4}>
-                    <ImageList sx={{ width: 120, height: 330 }} cols={1}>
+                <Box className="glass" p={2}>
+                    <ImageList sx={{ width: 100, height: 420 }} cols={1}>
                         {itemData.map((item) => (
                             <ImageListItem key={item.img}>
                                 <img
-                                    src={`${item.img}?w=150&fit=crop&auto=format`}
-                                    srcSet={`${item.img}?w=150&fit=crop&auto=format&dpr=2 2x`}
+                                    src={`${item.img}?w=100&fit=crop&auto=format`}
+                                    srcSet={`${item.img}?w=100&fit=crop&auto=format&dpr=2 2x`}
                                     alt={item.title}
                                     loading="lazy"
                                     onClick ={()=>{setTable2Texture(item.texture)}}
--- src/components/WoodenTable.jsx
+++ src/components/WoodenTable.jsx
@@ -1,6 +1,6 @@
 import { useGLTF, useTexture } from '@react-three/drei'
 import { useGlobalContext } from '../context'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { SRGBColorSpace, LinearSRGBColorSpace } from 'three';
 
 const WoodenTable = (props) => {
@@ -16,71 +16,65 @@ const WoodenTable = (props) => {
         aoMap: './wooden_table/wood_024_sd/aoMap.jpg',
     })
 
-    const texture3 = useTexture({
+    const texture2 = useTexture({
         map: './wooden_table/wood_022_sd/map.jpg',
         displacementMap: './wooden_table/wood_022_sd/height.png',
         normalMap: './wooden_table/wood_022_sd/normal.jpg',
         roughnessMap: './wooden_table/wood_022_sd/roughness.jpg',
         aoMap: './wooden_table/wood_022_sd/aoMap.jpg',
     })
-    const texture2 = useTexture({
+    const texture3 = useTexture({
         map: './wooden_table/wood_025_sd/map.jpg',
         displacementMap: './wooden_table/wood_025_sd/height.png',
         normalMap: './wooden_table/wood_025_sd/normal.jpg',
         roughnessMap: './wooden_table/wood_025_sd/roughness.jpg',
         aoMap: './wooden_table/wood_025_sd/aoMap.jpg',
     })
-
-    texture2.encoding = SRGBColorSpace; // Set the map to sRGB
-    texture2.displacementMap.colorSpace = LinearSRGBColorSpace;
-    texture2.normalMap.colorSpace = LinearSRGBColorSpace;
-    texture2.roughnessMap.colorSpace = LinearSRGBColorSpace;
-    texture2.aoMap.colorSpace = LinearSRGBColorSpace;
+    const [texture, setTexture] = useState(texture1)
 
     useEffect(() => {
-        texture2.encoding = SRGBColorSpace; // Set the map to sRGB
-        texture2.map.flipY = false
-        texture2.displacementMap.colorSpace = LinearSRGBColorSpace;
-        texture2.displacementMap.flipY = false
-        texture2.normalMap.colorSpace = LinearSRGBColorSpace;
-        texture2.normalMap.flipY = false
-        texture2.roughnessMap.colorSpace = LinearSRGBColorSpace;
-        texture2.roughnessMap.flipY = false
-        texture2.aoMap.colorSpace = LinearSRGBColorSpace;
-        texture2.aoMap.flipY = false
-    }, [])
-    useEffect(() => {
-        texture2.encoding = SRGBColorSpace; // Set the map to sRGB
-        texture2.map.flipY = false
-        texture2.displacementMap.colorSpace = LinearSRGBColorSpace;
-        texture2.displacementMap.flipY = false
-        texture2.normalMap.colorSpace = LinearSRGBColorSpace;
-        texture2.normalMap.flipY = false
-        texture2.roughnessMap.colorSpace = LinearSRGBColorSpace;
-        texture2.roughnessMap.flipY = false
-        texture2.aoMap.colorSpace = LinearSRGBColorSpace;
-        texture2.aoMap.flipY = false
-        materialRef.current.needUpdate = false
-    }, [table2Texture, texture2])
-    // const [texture, setTexture] = useState(texture1)
-    // useEffect(() => {
-    //     console.log(table2Texture)
-    //     if (table2Texture === 'texture1') { 
-    //         setTexture(texture1) 
-    //     }
-    //     if (table2Texture === 'texture2') { 
-    //         console.log(texture2)
-    //         setTexture(texture2) 
-    //     }
+        materialRef.current.map.colorSpace = SRGBColorSpace; // Set the map to sRGB
+        materialRef.current.map.flipY = false;
+        materialRef.current.map.needsUpdate = true;
 
-    // }, [table2Texture])
+        materialRef.current.displacementMap.colorSpace = LinearSRGBColorSpace;
+        materialRef.current.displacementMap.flipY = false;
+        materialRef.current.displacementMap.needsUpdate = false;
+
+        materialRef.current.normalMap.colorSpace = LinearSRGBColorSpace;
+        materialRef.current.normalMap.flipY = false;
+        materialRef.current.normalMap.needsUpdate = true;
+
+        materialRef.current.roughnessMap.colorSpace = LinearSRGBColorSpace;
+        materialRef.current.roughnessMap.flipY = false;
+        materialRef.current.roughnessMap.needsUpdate = true;
+
+        materialRef.current.aoMap.colorSpace = LinearSRGBColorSpace;
+        materialRef.current.aoMap.flipY = false;
+        materialRef.current.aoMap.needsUpdate = true;
+
+        materialRef.current.needsUpdate = true;
+    }, [table2Texture, texture])
+    
+    useEffect(() => {
+        if (table2Texture === 'texture1') {
+            setTexture(texture1)
+        }
+        if (table2Texture === 'texture2') {
+            setTexture(texture2)
+        }
+        if (table2Texture === 'texture3') {
+            setTexture(texture3)
+        }
+
+    }, [table2Texture])
 
 
     return (
         <group {...props} dispose={null}>
             <group rotation={[-Math.PI / 2, 0, 0]} scale={0.036}>
                 <mesh castShadow geometry={nodes.defaultMaterial.geometry} rotation={[Math.PI / 2, Math.PI * .25, 0]} >
-                    <meshStandardMaterial ref={materialRef} key={table2Texture} {...(table2Texture === "texture1" ? texture1 : texture2)} />
+                    <meshStandardMaterial ref={materialRef} key={table2Texture} {...texture} />
                 </mesh>
             </group>
         </group>
