4548c03fa98d2acc159560e08770cb9bdaa01641
Amir <amir.nadji@hotmail.com>
2023-09-11 21:55:36 +0100

    Reglages sofa 2

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -39,7 +39,7 @@ const Experience = () => {
             <Suspense fallback={<Html><h3>Chargement...</h3></Html>}>
                 {counter === 0 && <LazyTable2 rotation={[0, Math.PI * 0.25, 0]} />}
                 {counter === 1 && <LazyWoodenTable scale={0.8} />}
-                {counter === 2 && <LazySofa2 scale={0.8} rotation={[0, Math.PI * 0.25, 0]} />}
+                {counter === 2 && <LazySofa2 scale={1.2} rotation={[0, Math.PI * 0.25, 0]} position={[0, -0.5, 0]} />}
                 {counter === 3 && <LazyBahut scale={1.3} rotation={[0, -Math.PI * 0.9, 0]} position={[0, -0.5, 0]} />}
             </Suspense>
         </>
--- src/components/Sofa2.jsx
+++ src/components/Sofa2.jsx
@@ -1,4 +1,7 @@
 import { useGLTF, useTexture } from '@react-three/drei'
+import { useRef, useEffect } from 'react'
+import LevaComponent from './LevaComponent'
+import { SRGBColorSpace, LinearSRGBColorSpace } from 'three';
 
 const Sofa2 = (props) => {
 	const { nodes } = useGLTF('./sofa_visio/sofa_visio.glb')
@@ -13,15 +16,44 @@ const Sofa2 = (props) => {
 		map: './sofa_visio/diffuse2.jpg',
 		normalMap: './sofa_visio/normal2.jpg',
 		roughnessMap: './sofa_visio/roughness2.jpg',
-		metalicMap: './sofa_visio/metalic2.jpg',
+		metalnessMap: './sofa_visio/metalic2.jpg',
 	})
+	const { envMapIntensity } = LevaComponent()
+
+	const meshRef1 = useRef()
+	const meshRef2 = useRef()
+	const configTexture = (ref) => {
+		ref.current.map.colorSpace = SRGBColorSpace; // Set the map to sRGB
+		ref.current.map.flipY = false;
+		ref.current.map.needsUpdate = true;
+
+
+		ref.current.normalMap.colorSpace = LinearSRGBColorSpace;
+		ref.current.normalMap.flipY = false;
+		ref.current.normalMap.needsUpdate = true;
+
+		ref.current.roughnessMap.colorSpace = LinearSRGBColorSpace;
+		ref.current.roughnessMap.flipY = false;
+		ref.current.roughnessMap.needsUpdate = true;
+
+		ref.current.metalnessMap.colorSpace = LinearSRGBColorSpace;
+		ref.current.metalnessMap.flipY = false;
+		ref.current.metalnessMap.needsUpdate = true;
+
+		ref.current.needsUpdate = true;
+	}
+	useEffect(() => {
+		configTexture(meshRef1);
+		configTexture(meshRef2);
+
+	}, [])
 	return (
 		<group {...props} dispose={null}>
 			<mesh geometry={nodes.part_01.geometry} material={nodes.part_01.material} position={[0, 0.004, 0]} scale={0.01}>
-				<meshStandardMaterial {...texture1} />
+				<meshStandardMaterial ref={meshRef1} envMapIntensity={envMapIntensity} {...texture1} />
 			</mesh>
 			<mesh geometry={nodes.part_02.geometry} material={nodes.part_02.material} position={[-0.887, 0.393, 0]} scale={0.01}>
-				<meshStandardMaterial {...texture2} />
+				<meshStandardMaterial ref={meshRef2} envMapIntensity={envMapIntensity} {...texture2} />
 			</mesh>
 		</group>
 	)
